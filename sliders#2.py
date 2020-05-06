88dfd27cba7550efccc2398ab6b73d2649aa76e6
Qlas <rafgor7@gmail.com>
2020-05-06 19:55:24 +0200

    updated add user logic

--- sliders#2.py
+++ sliders#2.py
@@ -190,45 +190,50 @@ class AmountPeople(tk.Frame):
         self.n_entry.grid(row=self.user_count + 1, column=1, pady=(10, 10))
         self.user_count += 1
 
-
+w = 0
 class AddNewUser_PageOne(tk.Frame):
     def __init__(self, master):
         self.master = master
+        global w
         w = tk.IntVar()
         tk.Frame.__init__(self, master)
-        tk.Label(self, text="Zaznacz czy..", font=('Helvetica', 18, "bold")).grid(row=0, column=1)
-        tk.Radiobutton(self, text="Jesz mięso", variable=w, value=1).grid(row=1, column=1)
-        tk.Radiobutton(self, text="Jesz ryby", variable=w, value=2).grid(row=2, column=1)
-        tk.Radiobutton(self, text="Jesteś wegetarianinem", variable=w, value=3).grid(row=3, column=1)
-        tk.Radiobutton(self, text="Jesteś weganinem", variable=w, value=4).grid(row=4, column=1)
+        tk.Label(self, text="Imię", font=('Helvetica', 10)).grid(row=0, column=1)
+        self.n_entry = tk.Entry(self)
+        self.n_entry.grid(row=1, column=1, pady=(10, 10))
+        tk.Label(self, text="Zaznacz czy..", font=('Helvetica', 18, "bold")).grid(row=2, column=1)
+        tk.Radiobutton(self, text="Jesz mięso", variable=w, value=1).grid(row=3, column=1)
+        tk.Radiobutton(self, text="Jesz ryby", variable=w, value=2).grid(row=4, column=1)
+        tk.Radiobutton(self, text="Jesteś wegetarianinem", variable=w, value=3).grid(row=5, column=1)
+        tk.Radiobutton(self, text="Jesteś weganinem", variable=w, value=4).grid(row=6, column=1)
         tk.Button(self, text="Dalej",
-                  command=lambda: master.switch_frame(AddNewUser_PageTwo)).grid(row=5, column=2)
+                  command=lambda: master.switch_frame(AddNewUser_PageTwo)).grid(row=7, column=2)
         tk.Button(self, text="Powrót",
-                  command=lambda: master.switch_frame(StartPage)).grid(row=5, column=0)
+                  command=lambda: master.switch_frame(StartPage)).grid(row=7, column=0)
 
 
 class AddNewUser_PageTwo(tk.Frame):
     def __init__(self, master):
         self.master = master
-        w = tk.IntVar()
-        a = tk.IntVar()
-        b = tk.IntVar()
-        c = tk.IntVar()
-        d = tk.IntVar()
+        self.allergy = [tk.IntVar(), tk.IntVar(), tk.IntVar(), tk.IntVar()]
+
         tk.Frame.__init__(self, master)
         tk.Label(self, text="Jestem uczulony na ...", font=('Helvetica', 18, "bold")).grid(row=0, column=1)
-        rb1 = tk.Checkbutton(self, text="nabiał")
+        rb1 = tk.Checkbutton(self, text="nabiał", variable=self.allergy[0])
+
         rb1.grid(row=1, column=1)
-        tk.Checkbutton(self, text="orzechy").grid(row=2, column=1)
-        tk.Checkbutton(self, text="gluten").grid(row=3, column=1)
-        tk.Checkbutton(self, text="jajka").grid(row=4, column=1)
-        tk.Checkbutton(self, text="nie jestem uczulony", offvalue=5, variable=2).grid(row=4, column=1)
+        tk.Checkbutton(self, text="orzechy", variable=self.allergy[1]).grid(row=2, column=1)
+        tk.Checkbutton(self, text="gluten", variable=self.allergy[2]).grid(row=3, column=1)
+        tk.Checkbutton(self, text="jajka", variable=self.allergy[3]).grid(row=4, column=1)
         tk.Button(self, text="Dalej",
                   command=lambda: master.switch_frame(AddNewUser_PageThree)).grid(row=5, column=2)
         tk.Button(self, text="Powrót",
                   command=lambda: master.switch_frame(AddNewUser_PageOne)).grid(row=5, column=0)
         tk.Button(self, text="Wyczyść",
-                  command=lambda: self.rb1.set(None)).grid(row=5, column=1)
+                  command=self.reset).grid(row=5, column=1)
+
+    def reset(self):
+        for i in self.allergy:
+            i.set(0)
 
 
 
@@ -239,9 +244,7 @@ class AddNewUser_PageThree(tk.Frame):
         y = tk.DoubleVar()
         f = tk.DoubleVar()
         g = tk.DoubleVar()
-        tk.Label(self, text="Nazwa", font=('Helvetica', 10)).grid(row=0, column=1)
-        self.n_entry = tk.Entry(self)
-        self.n_entry.grid(row=1, column=1, pady=(10, 10))
+        tk.Label(self, text="Co wolisz bardziej", font=('Helvetica', 18, "bold")).grid(column=1)
         kryt1 = tk.Label(self, text="smak").grid(row=2, column=0)
         slider = tk.Scale(self, from_=-9, to=9, resolution=3, variable=y, orient=tk.HORIZONTAL)
         slider.grid(row=2, column=1)
@@ -262,7 +265,7 @@ class AddNewUser_PageThree(tk.Frame):
         tk.Button(self, text="zapisz",
                   command=lambda: master.switch_frame(StartPage)).grid(row=5, column=2)
         tk.Button(self, text="powrót",
-                  command=lambda: master.switch_frame(AddNewUser_PageOne)).grid(row=5, column=0)
+                  command=lambda: master.switch_frame(AddNewUser_PageTwo)).grid(row=5, column=0)
 
 
 if __name__ == '__main__':
