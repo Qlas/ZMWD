2c5df367238bc640bffd5a1f12a128f6decb1685
3Mydlo3 <mydlo333@interia.pl>
2020-05-07 11:02:29 +0200

    Extract matrix preparation to separate method

--- ahp.py
+++ ahp.py
@@ -3,18 +3,26 @@ import numpy as np
 class AHP:
     def __init__(self, sliders):
         # Prepare matrix
-        _sliders_count = len(sliders)
-        self.matrix = np.empty(shape=(_sliders_count, _sliders_count))
-        for _i in range(_sliders_count):
-            value = sliders[_i]
+        self.matrix = self._prepare_matrix(sliders)
+
+    def _prepare_matrix(self, criteria):
+        """
+        Input: 1D iterable with n numbers
+        Output: matrix nxn
+        """
+        _criteria_count = len(criteria)
+        matrix = np.empty(shape=(_criteria_count, _criteria_count))
+        for _i in range(_criteria_count):
+            value = criteria[_i]
             if value == 0:
                 value = 1
             elif value < 0:
                 value = self._inv(-value)
-            self.matrix[_i, _i] = 1
-            _j = _i + 1 if (_i + 1 < _sliders_count) else 0
-            self.matrix[_i, _j] = value
-            self.matrix[_j, _i] = self._inv(value)
+            matrix[_i, _i] = 1
+            _j = _i + 1 if (_i + 1 < _criteria_count) else 0
+            matrix[_i, _j] = value
+            matrix[_j, _i] = self._inv(value)
+        return matrix
 
     def _inv(self, value):
         """Inverts number eg. 9 to 1/9"""
