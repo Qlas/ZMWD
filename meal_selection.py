ae82858862fd6e09df0878dc081a47d4529943d7
3Mydlo3 <mydlo333@interia.pl>
2020-05-12 19:21:42 +0200

    Rename allergens to restrictions
    There is also meat!

--- meal_selection.py
+++ meal_selection.py
@@ -7,7 +7,7 @@ class MealSelection:
     def __init__(self, database, users_and_weights):
         self.database = database
         self.users, self.weights = self._extract_users_and_weights(users_and_weights)
-        self.allergens = self._get_all_allergens()
+        self.restrictions = self._get_all_restrictions()
 
     def _extract_users_and_weights(self, users_and_weights):
         users = []
@@ -17,7 +17,7 @@ class MealSelection:
             weights.append(float(weight.get()))
         return tuple(users), tuple(weights)
 
-    def _get_all_allergens(self):
+    def _get_all_restrictions(self):
         # If only one user is given then query must be altered a bit, otherwise it will throw an error
         query_filter = f"IN {self.users}" if len(self.users) > 1 else f"= '{self.users[0]}'"
         query = f"SELECT res_name, SUM(value) FROM users INNER JOIN user_res ON users.id=user_res.user_id WHERE users.name {query_filter} GROUP BY res_name"
