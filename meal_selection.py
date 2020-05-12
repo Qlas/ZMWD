e545d26ad9578b3e2333420b8a3898d38ab18009
3Mydlo3 <mydlo333@interia.pl>
2020-05-12 20:23:48 +0200

    Add some comments and cleanup

--- meal_selection.py
+++ meal_selection.py
@@ -11,6 +11,7 @@ class MealSelection:
         self.meals = self._get_all_meals()
 
     def _extract_users_and_weights(self, users_and_weights):
+        """Extracts data from [(usercombo, weightfield), ...]"""
         users = []
         weights = []
         for user, weight in users_and_weights:
@@ -19,11 +20,12 @@ class MealSelection:
         return tuple(users), tuple(weights)
 
     def _get_all_meals(self):
+        """Retrieves all meals from database"""
         query = "SELECT * FROM meals"
-        meals = pd.read_sql_query(query, self.database).set_index(['id'])
-        return meals
+        return pd.read_sql_query(query, self.database).set_index(['id'])
 
     def _get_all_restrictions(self):
+        """Retrieves informations about all possible restrictions for selected users"""
         # If only one user is given then query must be altered a bit, otherwise it will throw an error
         query_filter = f"IN {self.users}" if len(self.users) > 1 else f"= '{self.users[0]}'"
         query = f"SELECT res_name, SUM(value) FROM users INNER JOIN user_res ON users.id=user_res.user_id WHERE users.name {query_filter} GROUP BY res_name"
