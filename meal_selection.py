a38b0164d41eeedf6ae2301abeac089b3e36c876
3Mydlo3 <mydlo333@interia.pl>
2020-05-19 15:05:15 +0200

    Collect user preferences from AHP

--- meal_selection.py
+++ meal_selection.py
@@ -6,14 +6,15 @@ from ahp import AHP
 class MealSelection:
     def __init__(self, database, users_and_weights, meal_type):
         self.database = database.connect
-        self.users = [row[0] for row in users_and_weights]
-        self.weights = [row[1] for row in users_and_weights]
+        self.users = tuple([row[0] for row in users_and_weights])
+        self.weights = tuple([row[1] for row in users_and_weights])
         self.users_count = len(self.users)
         self.meat, self.allergens = self._get_all_restrictions()
         self.meals = self._get_all_meals()
         self.meals = self._apply_meal_restriction(meal_type)
         self.meals = self._apply_allergen_restrictions()
         self.meals = self._apply_meat_restrictions()
+        self.rate_meals()
 
     def _get_all_meals(self):
         """Retrieves all meals from database"""
@@ -53,3 +54,16 @@ class MealSelection:
             if (float(self.meat.loc[vege_type]) >= 1):
                 filtered_meals = filtered_meals.loc[filtered_meals[vege_type] >= 1]
         return filtered_meals
+
+    def rate_meals(self):
+        """Handles meal rating procedure"""
+        # If only one user is given then query must be altered a bit, otherwise it will throw an error
+        query_filter = f"IN {self.users}" if len(self.users) > 1 else f"= '{self.users[0]}'"
+        # Get data about user preferences
+        query = f"SELECT users.name, ahp_pref_name, value FROM users INNER JOIN user_ahp_pref ON users.id=user_ahp_pref.user_id WHERE users.name {query_filter}"
+        result = pd.read_sql_query(query, self.database).set_index(['name'])
+        # Calculate meal rating for each user
+        for user in self.users:
+            user_raw_preferences = list(result.loc[user]['value'])
+            user_AHP = AHP(*user_raw_preferences)
+            user_preferences = user_AHP.get_preferences()
